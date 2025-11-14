4441f83ef0687ec5c6be72b8e15100433c4fa6aa
altayelmas <altayelmas@gmail.com>
2025-11-14 09:07:36 +0300

    Update assembly informational version and cache files for multiple services

--- debt_payment_backend/IdentityService/obj/Debug/net8.0/IdentityService.AssemblyInfo.cs
+++ debt_payment_backend/IdentityService/obj/Debug/net8.0/IdentityService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+702442669e5055d9a20b7da3654a1776db8da9ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b0eda008200a7dd3efcb78087c8b1a8f1fb007f")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
